d80765912fa7aa3a045e874dc2cbcc4da7b8ecbe
Tejalp21 <tejalpardeshi21@gmail.com>
2023-10-11 20:01:20 +0530

    springboot jwt 29 sep

---
+++ src/main/java/com/prowings/SpringbootJwtPractical29SepApplication.java
@@ -0,0 +1,13 @@
+package com.prowings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtPractical29SepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJwtPractical29SepApplication.class, args);
+	}
+
+}
